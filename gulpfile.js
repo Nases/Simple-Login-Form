ac27eeb4aaa275e8ec8497d2e000c79579078984
Hasan Sefa Ozalp <30827573+Nases@users.noreply.github.com>
2020-02-06 20:42:21 -0800

    remove redundancy

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require("gulp");
-const browserSync = require("browser-sync").create();
-
-gulp.task("default", function() {
-  browserSync.init({
-    server: "./"
-  });
-  gulp.watch("./*").on("change", browserSync.reload);
-});
